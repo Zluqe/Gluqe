ad8414062416108e3178afddf86f194e0639201a
Person0z <80363995+Person0z@users.noreply.github.com>
2025-01-07 00:40:59 -0600

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -18,10 +18,6 @@ class Moderation(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        print(f'Loaded Cog Moderation')
-
-    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
@@ -57,36 +53,6 @@ class Moderation(commands.Cog):
             await message.delete()
             await message.channel.send(warning_msg, delete_after=15)
             return
-
-    def save_blacklist(self, blacklist):
-        with open('data/blacklist.json', 'w') as f:
-            json.dump(blacklist, f)
-
-    @commands.hybrid_command(name="blacklist")
-    @is_owner()
-    async def blacklist(self, ctx, user: discord.User):
-        """
-        Blacklist a user from using the bot.
-        """
-        try:
-            blacklist = load_blacklist()
-            if user.id not in blacklist:
-                blacklist.append(user.id)
-                self.save_blacklist(blacklist)
-                await ctx.reply(f"{user.name} has been blacklisted.")
-            else:
-                try:
-                    blacklist = load_blacklist()
-                    if user.id in blacklist:
-                        blacklist.remove(user.id)
-                        self.save_blacklist(blacklist)
-                        await ctx.reply(f"{user.name} has been removed from the blacklist.")
-                    else:
-                        await ctx.reply(f"Legacy Code, If i remove everything will break.")
-                except Exception as e:
-                    print(e)                
-        except Exception as e:
-            print(e)
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
